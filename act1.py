1ba4984badd659b62a3a95149d7579322600089f
razaiqbal2 <duaf0226@gmail.com>
2024-12-26 17:54:19 +0500

    number catching even or odd

---
+++ act1.py
@@ -0,0 +1,10 @@
+number=int(input("Enter your number :"))
+print("Check to enter again :" , number)
+
+if number%2 == 0 :
+    print('This is an even number ')
+
+else :
+    print('this is an odd number')
+
+
