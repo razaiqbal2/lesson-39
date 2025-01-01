08eab53f60d7cef1cdc0f46dd4ea0a6e00680b47
razaiqbal2 <duaf0226@gmail.com>
2025-01-01 15:46:40 +0500

    repeat

---
+++ act3.py
@@ -0,0 +1,10 @@
+num = int(input("Please enter your number here : "))
+
+if num > 50:
+    print("This is greater than 50")
+if num %2==0:
+        print("This is a even also ")
+else:
+        print("This is an odd number")
+
+         
---
+++ act4.py
@@ -0,0 +1,11 @@
+import datetime
+
+current_time=datetime.datetime.now()
+
+print("Time now at karachi is")
+print(current_time)
+
+
+import calendar
+
+print("\n",calendar.calendar(2009))
