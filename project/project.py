8c6380c93dca7f13b1411e7cfec18ef9b770a962
razaiqbal2 <duaf0226@gmail.com>
2025-01-01 16:09:06 +0500

    exams

---
+++ project/project.py
@@ -0,0 +1,8 @@
+fees=float(input("Is child is eligibe for exams : "))
+
+
+if fees < 2500:
+    print("Your child is not eligible for exams ")
+else:
+    print("Your child is eligible for exams")
+
